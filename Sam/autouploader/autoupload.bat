6b60aaacb397d95633b0d5652a61680ae49aee38
smikkelbakje <59176120+smikkelbakje@users.noreply.github.com>
2019-12-24 02:08:03 +0100

    added information

--- Sam/autouploader/autoupload.bat
+++ Sam/autouploader/autoupload.bat
@@ -1,7 +1,12 @@
+#going to the drive insert your own path
 E:
 cd git
+#getting updates from the repo
 git pull
+#uploading
 git add .
+# you can change (hoe wil je de wijzigingen noemen)
 set /P commit="hoe wil je de wijziging noemen?"
 git commit -m "%commit%"
 git push
+#uploading done
