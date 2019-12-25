c9407c43c6590287d534d1ab89fe9ed2b9ee1b4c
smikkelbakje <samhos2006@hotmail.com>
2019-12-25 12:01:15 +0100

    work in progress project

--- Sam/1. Batch Windows/1. autouploader/autoupload.bat
+++
@@ -1,9 +0,0 @@
-E:
-cd git
-git pull
-git add .
-set /P commit="hoe wil je de wijziging noemen?"
-git commit -m "%commit%"
-git push
-@echo als het niet heefd gewerkt moet je alle commandos zelf invoeren in een shell
-pause
---
+++ Sam/2. Python/(WIP) projects/WIP programma.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+
+print("hallo wat is je naam")
+name = input()
+groet = ("welkom bij dit zelf gemaakte programma ")
+print(groet + name)
+print("wat wil je doen")
+com = input()
+if com == 'help':
+ print("")
+ print("dit is help centrum")
+ print("programma openen")
+ 
+elif com == 'programma':
+ print("")
+ print("welk programma wil je openen")
+ 
