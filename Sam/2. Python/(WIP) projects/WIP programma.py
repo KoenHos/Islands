dbc5a2b0ecc542fb7257cc926fae0912b1f7ca71
smikkelbakje <samhos2006@hotmail.com>
2019-12-25 12:10:06 +0100

    worked on the wip file

--- Sam/2. Python/(WIP) projects/WIP programma.py
+++ Sam/2. Python/(WIP) projects/WIP programma.py
@@ -1,12 +1,13 @@
 import os
-import subprocess
 
 print("hallo wat is je naam")
 name = input()
 groet = ("welkom bij dit zelf gemaakte programma ")
 print(groet + name)
+
 print("wat wil je doen")
 com = input()
+
 if com == 'help':
  print("")
  print("dit is help centrum")
@@ -14,5 +15,6 @@ if com == 'help':
  
 elif com == 'programma':
  print("")
- print("welk programma wil je openen")
- 
+ print("welk programma wil je openen geef de naam in het engels")
+ prog = input()
+ os.system(prog + ".exe") 
